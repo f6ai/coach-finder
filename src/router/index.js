81b386523f52265d2d8c614b2e4ba164e028eca7
Fanni Szikszai <fanniszikszai@gmail.com>
2026-03-17 11:42:20 +0100

    add initial components

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import CoachDetails from '@/pages/coaches/CoachDetails.vue';
+import CoachesList from '@/pages/coaches/CoachesList.vue';
+import CoachRegister from '@/pages/coaches/CoachRegister.vue';
+import ContactCoach from '@/pages/requests/ContactCoach.vue';
+import RequestsReceived from '@/pages/requests/RequestsReceived.vue';
+import NotFound from '@/pages/NotFound.vue';
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id', component: CoachDetails, children: [
+
+        { path: '/contact', component: ContactCoach },
+      ]
+    },
+    { path: '/register', component: CoachRegister },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: NotFound },
+  ]
+})
+
+export default router
