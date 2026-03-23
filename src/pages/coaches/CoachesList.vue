03e9c77eb5cfcd562e531d6674799d1481d94a83
Fanni Szikszai <fanniszikszai@gmail.com>
2026-03-23 12:48:49 +0100

    add coach filter

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,6 +1,6 @@
 <template>
     <section>
-        FILTER
+        <CoachFilter @change-filter="setFilters" </CoachFilter>
     </section>
     <section>
         <BaseCard>
@@ -20,18 +20,47 @@
 
 <script>
 import CoachItem from '@/components/coaches/CoachItem.vue';
+import CoachFilter from './CoachFilter.vue';
 
 export default {
     components: {
-        CoachItem
+        CoachItem,
+        CoachFilter
+    },
+    data() {
+        return {
+            activeFilters: {
+                frontend: true,
+                backend: true,
+                career: true
+            }
+        }
     },
     computed: {
         filteredCoaches() {
             // coaches/coaches -> first is the namespace second is the getter name
-            return this.$store.getters['coaches/coaches']
+            const coaches = this.$store.getters['coaches/coaches']
+            return coaches.filter(coach => {
+                console.log(coach)
+                if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
+                    return true
+                }
+                if (this.activeFilters.backend && coach.areas.includes('backend')) {
+                    return true
+                }
+                if (this.activeFilters.career && coach.areas.includes('career')) {
+                    return true
+                }
+                return false
+            })
         },
         hasCoaches() {
             return this.$store.getters['coaches/hasCoaches']
+        }
+    },
+    methods: {
+        setFilters(updatedFilters) {
+            this.activeFilters = updatedFilters;
         }
     }
 }
