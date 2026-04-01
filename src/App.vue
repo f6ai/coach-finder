0e92f1c39bf223d8f7625b1a4a3f0736b4fd8d65
Fanni Szikszai <fanniszikszai@gmail.com>
2026-04-01 12:10:16 +0200

    add auto login and logout

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,22 @@ import TheHeader from './components/layout/TheHeader.vue';
 export default {
     components: {
         TheHeader
+    },
+    computed: {
+        didAutoLogout() {
+            return this.$store.getters.didAutoLogoout
+        }
+    },
+    created() {
+        this.$store.dispatch('tryLogin')
+    },
+    watch: {
+        didAutoLogout(curValue, oldValue) {
+            if (curValue && curValue !== oldValue) {
+                this.$router.replace('/coaches')
+            }
+
+        }
     }
 }
 </script>
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,35 +1,27 @@
-import { normalizeModuleId } from "vite/module-runner";
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-};
+}
+
+let timer
 
 export default {
     async login(context, payload) {
-        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseConfig.apiKey}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        })
-
-        const responseData = await res.json()
-
-        if (!res.ok) {
-            const error = new Error(`Failed to log in ${responseData.error.message}`)
-            throw error
-        }
-
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         })
     },
     async signup(context, payload) {
-        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseConfig.apiKey}`, {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode
+        const url = mode === 'signup' ? `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebaseConfig.apiKey}` : `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseConfig.apiKey}`
+
+        const res = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -45,17 +37,59 @@ export default {
             throw error
         }
 
+        // determine when token expires in the future
+        const expiresIn = +responseData.expiresIn * 1000;
+        const expirationDate = new Date().getTime() + expiresIn
+
+        localStorage.setItem('token', responseData.idToken)
+        localStorage.setItem('userId', responseData.localId)
+        localStorage.setItem('tokenExpiration', expirationDate)
+
+        timer = setTimeout(function () {
+            context.dispatch('autoLogout')
+        }, expiresIn)
+
         context.commit('setUser', {
             token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+            userId: responseData.localId
         })
     },
+    tryLogin(context) {
+        const token = localStorage.getItem('token')
+        const userId = localStorage.getItem('userId')
+        const tokenExpiration = localStorage.getItem('tokenExpiration')
+
+        const expiresIn = +tokenExpiration - new Date().getTime()
+
+        if (expiresIn < 0) {
+            return
+        }
+
+        timer = setTimeout(function () {
+            context.dispatch('autoLogout')
+        }, expiresIn)
+
+        if (token && userId) {
+            context.commit('setUser', {
+                token,
+                userId
+            })
+        }
+    },
     logout(context) {
+        localStorage.removeItem('token')
+        localStorage.removeItem('userId')
+        localStorage.removeItem('tokenExpiration')
+
+        clearTimeout(timer)
+
         context.commit('setUser', {
             token: null,
-            userId: null,
-            tokenExpiration: null
+            userId: null
         })
+    },
+    autoLogout(context) {
+        context.dispatch('logout');
+        context.commit('setAutoLogout');
     }
 }
